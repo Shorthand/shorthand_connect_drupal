35cbee30f9dc651d87a6a49a805dfc41b28d437e
Andrew Dekker <simultech@gmail.com>
2018-01-22 12:25:32 -0800

    Register display template in module
    Register the drupal templates in the module

--- node--shorthand-story.tpl.php
+++ node--shorthand-story.tpl.php
@@ -6,8 +6,24 @@
  */
 ?>
 
-<div>
-    <div class="content clearfix"<?php print $content_attributes; ?>>
+<div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+
+  <?php print render($title_prefix); ?>
+  <?php if (!$page): ?>
+    <h2<?php print $title_attributes; ?>>
+      <a href="<?php print $node_url; ?>"><?php print $title; ?></a>
+    </h2>
+  <?php endif; ?>
+  <?php print render($title_suffix); ?>
+
+  <?php if ($display_submitted): ?>
+    <div class="meta submitted">
+      <?php print $user_picture; ?>
+      <?php print $submitted; ?>
+    </div>
+  <?php endif; ?>
+
+  <div class="content clearfix"<?php print $content_attributes; ?>>
 
       <?php if(!empty($story_id)): ?>
         <?php print $story_body; ?>
@@ -15,4 +31,22 @@
       <?php endif; ?>
 
     </div>
+
+  <?php
+    // Remove the "Add new comment" link on the teaser page or if the comment
+    // form is being displayed on the same page.
+    if ($teaser || !empty($content['comments']['comment_form'])) {
+      unset($content['links']['comment']['#links']['comment-add']);
+    }
+    // Only display the wrapper div if there are links.
+    $links = render($content['links']);
+    if ($links):
+  ?>
+    <div class="link-wrapper">
+      <?php print $links; ?>
+    </div>
+  <?php endif; ?>
+
+  <?php print render($content['comments']); ?>
+
 </div>
